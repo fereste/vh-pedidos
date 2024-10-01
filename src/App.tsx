a9e42b291ffdba9c327e9b0220908e5b6e68203c
fereste <fernando.piubel@gmail.com>
2024-10-01 20:42:59 -0300

    Primera versión app pedidos

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,32 @@
-import { Button, Container, CssBaseline, FormControl, InputLabel, MenuItem, Select, Stack, Typography } from '@mui/material'
+import { Container, createTheme, CssBaseline, Paper, ThemeProvider, Typography } from '@mui/material'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { useState } from 'react';
+import { OrderForm } from './OrderForm';
 import axios from 'axios';
+import { green } from '@mui/material/colors';
 
-const products = [
-  { id: 1, name: 'Café' },
-  { id: 2, name: 'Té' },
-  { id: 3, name: 'Café con medialunas' },
-];
+axios.defaults.headers.common['Content-Type'] = 'application/json';
 
-function App() {
-  const [product, setProduct] = useState<string>('')
+const darkTheme = createTheme({ palette: { mode: 'dark', primary: green } });
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    const form = new FormData(event.currentTarget)
-    const productId = Number(form.get('product'))
-    setProduct(products.find(product => product.id === productId)?.name.toLowerCase() || '')
-
-    axios.post(
-      'http://localhost:3000/orders',
-      { id: productId },
-      { headers: { 'Content-Type': 'application/json' } }
-    )
-      .then(response => console.log(response))
-      .catch(error => console.error(error))
-  }
-
-  return (
-    <>
-      <CssBaseline />
-      <Container maxWidth="sm" sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '100vh' }}>
-        <Typography component="h1" variant="h4" sx={{ mb: 3 }}>
-          Cafetería UNLaM
-        </Typography>
-        <form onSubmit={handleSubmit}>
-          <Stack spacing={2}>
-            <FormControl fullWidth>
-              <InputLabel id="product-label" variant='outlined'>¿Qué querés pedir?</InputLabel>
-              <Select
-                name='product'
-                labelId="product-label"
-                id="product"
-                label="¿Qué querés pedir?"
-              >
-                {products.map(product => (
-                  <MenuItem key={product.id} value={product.id}>{product.name}</MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-            <Button type='submit' variant="contained" color="primary" fullWidth>
-              Hacer pedido
-            </Button>
-            <Typography variant="body2">
-              {product ? `Se pidió un ${product}` : '¡Hacé tu pedido!'}
-            </Typography>
-          </Stack>
-        </form>
-      </Container>
-    </>
-  )
+const App: React.FC = () => {
+    return (
+        <>
+            <CssBaseline />
+            <ThemeProvider theme={darkTheme}>
+                <Paper elevation={0}>
+                    <Container maxWidth="sm" sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '100vh' }}>
+                        <Typography component="h1" variant="h4" sx={{ mb: 3 }}>
+                            Cafetería UNLaM
+                        </Typography>
+                        <OrderForm />
+                    </Container>
+                </Paper>
+            </ThemeProvider>
+        </>
+    );
 }
 
 export default App
---
+++ src/OrderForm.tsx
@@ -0,0 +1,75 @@
+import { useState } from 'react';
+import { Button, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, Stack, Typography } from '@mui/material'
+import axios from 'axios';
+
+interface Product {
+    id: number;
+    name: string;
+}
+
+const products: Product[] = [
+    { id: 1, name: 'Café' },
+    { id: 2, name: 'Té' },
+    { id: 3, name: 'Café con medialunas' },
+];
+
+export const OrderForm = () => {
+    const [product, setProduct] = useState<Product>(products[0]);
+    const [orderPlaced, setOrderPlaced] = useState(false);
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+
+        axios.post('http://localhost:3000/orders', {
+            id: product.id,
+            productName: product.name,
+        })
+            .then(response => {
+                console.log(response);
+                setOrderPlaced(true);
+            })
+            .catch(error => console.error(error));
+    }
+
+    const handleProductChange = (event: SelectChangeEvent<number>) => {
+        const id = event.target.value;
+        const product = products.find(product => product.id === id);
+        if (product) {
+            setProduct(product);
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <Stack spacing={2}>
+                <FormControl fullWidth>
+                    <InputLabel id="product-label" variant='outlined'>¿Qué querés pedir?</InputLabel>
+                    <Select<number>
+                        required
+                        name='product'
+                        labelId="product-label"
+                        id="product"
+                        label="¿Qué querés pedir?"
+                        value={product.id}
+                        onChange={handleProductChange}
+                    >
+                        {products.map(product => (
+                            <MenuItem
+                                key={product.id}
+                                value={product.id}
+                            >
+                                {product.name}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+                <Button type='submit' variant="contained" color="primary" fullWidth>
+                    Hacer pedido
+                </Button>
+                <Typography variant="body1">
+                    {orderPlaced ? `Se pidió un ${product.name}` : '¡Hacé tu pedido!'}
+                </Typography>
+            </Stack>
+        </form>
+    );
+}
