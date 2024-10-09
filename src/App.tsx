6689e3329d866fb197f16ee5e0b65b660c9325ef
fereste <fernando.piubel@gmail.com>
2024-10-08 22:22:47 -0300

    Agrego año

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ const App: React.FC = () => {
                             sx={{ mb: 3 }}
                         >
                             <Typography component="h1" variant="h4">
-                                Cafetería UNLaM
+                                Cafetería UNLaM 2024
                             </Typography>
                             <Typography variant='body2' color='textSecondary'>
                                 {import.meta.env.BUILD_DATE}
